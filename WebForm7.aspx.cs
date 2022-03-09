ce919ee2323e74b8ff96d6fa06045aa9ea64fa14
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-09 22:02:30 +0530

    Add files via upload

---
+++ WebForm7.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm7 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int n1, n2, res=0;
+            n1 = Convert.ToInt32(TextBox1.Text);
+            n2 = Convert.ToInt32(TextBox2.Text);
+
+            if (n1 < n2)
+            {
+                res = n1;
+            }
+            else
+            {
+                res = n2;
+            }
+
+            Label1.Text = "smaller number=" + res;
+
+        }
+    }
+}
