b6d29509cbdc394359559f7595d63f46e1710709
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-09 22:02:49 +0530

    Add files via upload

---
+++ WebForm8.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm8 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int n1;
+            n1 = Convert.ToInt32(TextBox1.Text);
+
+            string day=TextBox1.Text;
+            switch(n1)
+            {
+                case 1:
+                    day = "Monday";
+                    break;
+                case 2:
+                    day = "Tuesday";
+                    break;
+                case 3:
+                    day = "Wednesday";
+                    break ;
+                case 4:
+                    day = "Thursday";
+                    break;
+                case 5:
+                    day = "Friday";
+                    break;
+                case 6:
+                    day = "Saturday";
+                    break;
+                case 7:
+                    day = "Sunday";
+                    break;
+                default:
+                    day = "Invalid no";
+                    break;
+            }
+
+            Label1.Text ="Dayname="+ day;
+        }
+    }
+}
