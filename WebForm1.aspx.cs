550e38643eeff403c518bdda00c170128c6713c2
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-09 21:59:35 +0530

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int n1;
+            n1 = Convert.ToInt32(TextBox1.Text);//(Property/Value)
+            int sq = n1 * n1;
+            Label1.Text = "Square=" + sq;
+        }
+    }
+}
