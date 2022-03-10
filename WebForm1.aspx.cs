ea9f31a25c74c5e71083071a541193d38fa89639
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-10 22:05:22 +0530

    Add files via upload

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace WebApplication1
+namespace WebApplication2
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
@@ -16,10 +16,24 @@ namespace WebApplication1
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            int n1;
-            n1 = Convert.ToInt32(TextBox1.Text);//(Property/Value)
-            int sq = n1 * n1;
-            Label1.Text = "Square=" + sq;
+            Label1.Text = "Username:-" + TextBox1.Text;
+            Label2.Text = "Password:-" + TextBox2.Text;
+            Label3.Text = "Address:-" + TextBox3.Text;
+            Label4.Text = "DOB:-" + TextBox4.Text;
+            Label5.Text = "Email:-:" + TextBox5.Text;
+
+        }
+
+        protected void LinkButton2_Click(object sender, EventArgs e)
+        {
+            //write code to go to register page
+
+            //Response.Redirect("WebForm1.aspx");
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("home.aspx");
         }
     }
 }
