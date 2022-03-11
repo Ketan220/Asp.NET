f1c130fcc210abba1f6162c2177f7bdd91be8386
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-11 16:19:41 +0530

    Add files via upload

--- WebForm5.aspx.cs
+++ WebForm5.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace WebApplication1
+namespace WebApplication2
 {
     public partial class WebForm5 : System.Web.UI.Page
     {
@@ -16,34 +16,7 @@ namespace WebApplication1
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            int m1, m2, m3, total = 0;
-            float per;
-            m1 = Convert.ToInt32(TextBox1.Text);
-            m2 = Convert.ToInt32(TextBox1.Text);
-            m3 = Convert.ToInt32(TextBox1.Text);
-
-            total = m1 + m2 + m3;
-            per = ((total / 0.3f) * 0.1f);
-
-
-            string res=Label2.Text;
-            if (per >= 75 && per <= 100)
-            {
-                res="A";
-            }
-            else if (per >= 50 && per < 75)
-            {
-                res="B";
-            }
-
-
-
-            Label1.Text = "Total marks="+total;
-            Label2.Text = "Percentage=" + per;
-            Label3.Text = "Grade=" + res;
-        
-           
-
+            Label1.Text = " Username=" + TextBox1.Text;
         }
     }
 }
