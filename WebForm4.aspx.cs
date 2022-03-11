9aa509ee9ba20a1599ea0e828a331da7142fdcc6
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-11 16:19:26 +0530

    Add files via upload

--- WebForm4.aspx.cs
+++ WebForm4.aspx.cs
@@ -5,40 +5,18 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace WebApplication1
+namespace WebApplication2
 {
     public partial class WebForm4 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Label1.Text = DateTime.Now.ToString();
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            int n1, n2, res = 0;
-            n1=Convert.ToInt32(TextBox1.Text);
-            n2=Convert.ToInt32(TextBox2.Text);
-            string ch = TextBox3.Text;
-
-            if(ch== "+")
-            {
-                res = n1 + n2;
-            }
-            if(ch== "-")
-            {
-                res = n1 - n2;
-            }
-            if(ch=="*")
-            {
-                res=n1 * n2;
-            }
-            if(ch=="/")
-            {
-                res = n1 / n2;
-            }
-
-            Label1.Text = "Result=" + res;
+            Label2.Text = TextBox1.Text;
         }
     }
 }
