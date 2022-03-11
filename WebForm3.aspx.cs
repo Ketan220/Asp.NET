b9f415a03246ee6f1c0d3e96028da35f61973492
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-11 16:18:51 +0530

    Add files via upload

--- WebForm3.aspx.cs
+++ WebForm3.aspx.cs
@@ -5,42 +5,13 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace WebApplication1
+namespace WebApplication2
 {
     public partial class WebForm3 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
-        }
-
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            int n1, n2, res = 0;
-            n1=Convert.ToInt32(TextBox1.Text);
-            n2=Convert.ToInt32(TextBox2.Text);
-            string ch = TextBox3.Text;
-
-            switch (ch)
-            {
-                case "+":
-                    res=n1+ n2;
-                    break;
-
-                case "-":
-                    res = n1 - n2;
-                    break;
-
-                case "*":
-                    res = n1 * n2;
-                    break;
-
-                case "/":
-                    res = n1 / n2;
-                    break;
-            }
-
-            Label1.Text = "Result=" + res;
         }
     }
 }
