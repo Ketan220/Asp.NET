9169ddf9db1639d186d00b88d05cf7bdb51e0042
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-15 21:47:26 +0530

    Add files via upload

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -5,7 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace WebApplication2
+using System.Data.SqlClient;
+
+namespace WebApplication3
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
@@ -14,26 +16,140 @@ namespace WebApplication2
 
         }
 
+        //create an object of sqlconnection class for creating 
+        //connection between this web page and sqlserver 
+
+        static string cn = "data source=DESKTOP-JEOJBC2\\SQLEXPRESS;Integrated security=true;database=test";
+
+        SqlConnection con = new SqlConnection(cn);
+       
+        
         protected void Button1_Click(object sender, EventArgs e)
         {
-            Label1.Text = "Username:-" + TextBox1.Text;
-            Label2.Text = "Password:-" + TextBox2.Text;
-            Label3.Text = "Address:-" + TextBox3.Text;
-            Label4.Text = "DOB:-" + TextBox4.Text;
-            Label5.Text = "Email:-:" + TextBox5.Text;
+
+            try
+            {
+                string qr = "insert into student(rno,name) values('" + TextBox1.Text + "','" + TextBox2.Text + "')";
+                SqlCommand command = new SqlCommand(qr, con);
+
+                //in connected environment you have to open the connection first
+                con.Open();
+
+                //execute the insert query using executenonquery method of sqlcommand class
+                command.ExecuteNonQuery();
+                Label1.Text = "Record saved Sucessfully.";
+                TextBox1.Text = "";
+                TextBox2.Text = "";
+                TextBox1.Focus();
+            }
+            catch (Exception ex)
+            {
+                Label1 .Text=ex.ToString();
+            }
+            finally
+            {
+                con.Close();
+            }
 
         }
 
-        protected void LinkButton2_Click(object sender, EventArgs e)
+        protected void Button2_Click(object sender, EventArgs e)
         {
-            //write code to go to register page
+            try
+            {
 
-            //Response.Redirect("WebForm1.aspx");
+                string qr = "update  student set name='" + TextBox2.Text + "' where rno='" + TextBox1.Text + "'";
+                SqlCommand command = new SqlCommand(qr, con);
+
+                //in connected environment you have to open the connection first
+                con.Open();
+
+                //execute the insert query using executenonquery method of sqlcommand class
+                command.ExecuteNonQuery();
+                Label1.Text = "Record updated successfully";
+                TextBox1.Text = "";
+                TextBox2.Text = "";
+                TextBox1.Focus();
+
+            }
+            catch (Exception ee)
+            {
+                Label1.Text = ee.ToString();
+            }
+            finally
+            {
+                //close the connection
+                con.Close();
+            }
+
+
         }
 
-        protected void LinkButton1_Click(object sender, EventArgs e)
+        protected void Button3_Click(object sender, EventArgs e)
         {
-            Response.Redirect("home.aspx");
+           
+            string qr="delete from student where name='"+TextBox2.Text+"' or rno='"+TextBox1.Text+"'";
+            SqlCommand command = new SqlCommand(qr,con);
+
+            try
+            {
+
+                //in connected environment you have to open the connection first
+                con.Open();
+
+                //execute the insert query using executenonquery method of sqlcommand class
+                command.ExecuteNonQuery();
+                Label1.Text = "record deleted successfully";
+                TextBox1.Text = "";
+                TextBox2.Text = "";
+                TextBox1.Focus();
+
+            }
+            catch (Exception ee)
+            {
+                Label1.Text=ee.ToString();  
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string qr = "select * from student where rno='" + TextBox1.Text + "'";
+                SqlCommand command = new SqlCommand(qr, con);
+
+                con.Open();
+
+                SqlDataReader dr =command.ExecuteReader();
+                int flag = 0;
+                while(dr.Read())
+                {
+                    Label1.Text = "";
+                    flag = 1;
+                    TextBox2.Text=dr["name"].ToString();
+
+                }
+                if(flag==0)
+                {
+                    Label1.Text = "No record found";
+                }
+                dr.Close();
+                
+            }
+            catch(Exception ee)
+            {
+                Label1.Text= ee.ToString();
+            }
+            finally
+            {
+                con.Close();
+            }
         }
     }
 }
