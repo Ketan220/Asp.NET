937ae3af82d9bd8192f4402f95211f1a20800722
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-23 16:14:58 +0530

    Add files via upload

---
+++ querystring2.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication11
+{
+    public partial class querystring2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //code to retrieve query string value 
+            string username=Request.QueryString["uname"].ToString();
+            Label1.Text = username;
+        }
+    }
+}
