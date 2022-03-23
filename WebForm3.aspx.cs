6b44d24d02aeb2dff7a4be8ef346a1d7a2a32fb5
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-23 16:16:58 +0530

    Add files via upload

--- WebForm3.aspx.cs
+++ WebForm3.aspx.cs
@@ -5,12 +5,21 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace WebApplication2
+namespace WebApplication13
 {
     public partial class WebForm3 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //code to retrieve session variables value
+            string u = Session["username"].ToString();
+            string em = Session["email"].ToString();
+            int uc = Convert.ToInt32(Session["usercount"].ToString());
+            uc = uc + 1;
+            Session["usercount"] = uc;
+            Label1.Text = u;
+            Label2.Text = em;
+            Label3.Text = uc.ToString();
 
         }
     }
