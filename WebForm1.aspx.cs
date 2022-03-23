7040abad3f0acee7b30c73b8cbe4f900552fe3e0
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-23 16:15:22 +0530

    Add files via upload

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -5,13 +5,31 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace WebApplication11
+namespace WebApplication12
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            String cn = (String)Application["companyname"];
+            String ct = (String)Application["companycity"];
+            int cnt = (int)Application["counter"];
+            cnt = cnt + 1;
+            Response.Write("you are visitor no.=" + cnt.ToString());
+            Response.Write("<hr>company name= " + cn);
+            Response.Write("<hr>company city=" + ct);
 
+            Application["counter"] = cnt;
+            
+            //reading session varible values
+            string uname=Session["uname"].ToString();
+            int ucnt = (int)Session["usercount"];
+            ucnt = ucnt + 1;
+            Session["usercount"] = ucnt;
+            Response.Write("<hr>User visiting this page=" + ucnt.ToString());
+            Response.Write("<br>uname=" + uname);
+
+           
         }
     }
 }
