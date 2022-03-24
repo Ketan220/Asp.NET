5a5ed916b6d5fb5bcbf8033f4d9d91aa2c65b94f
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-24 21:52:09 +0530

    Add files via upload

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -5,18 +5,13 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace WebApplication15
+namespace WebApplication16
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
-        }
-
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            Label1.Text = "username:" + TextBox1.Text;
         }
     }
 }
