b435412e5418f0d842ed3e4bbff6765959b8f3ab
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-24 21:51:19 +0530

    Add files via upload

--- WebForm2.aspx.cs
+++ WebForm2.aspx.cs
@@ -5,28 +5,71 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace WebApplication13
+namespace WebApplication15
 {
     public partial class WebForm2 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string cn=Application["companyname"].ToString();
-            string ad=Application["address"].ToString();
-            int cnt = (int)Application["count"];
-            cnt++;
-            Application["count"] = cnt;
-            Response.Write("welcome to" + cn +"<br>");
-            Response.Write("at" + ad +"<br>");
-            Response.Write("no. of count" + cnt.ToString() + "<br>");
+
+        }
+
+        protected void CustomValidator1_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            string val=args.Value;
+            int len=args.Value.Length;
+            Response.Write("Value=" + val+"<br>");
+            Response.Write("length= " + len);
+            if (len==10)
+            {
+                string d=val.Substring(0,2);
+                int dd=Convert.ToInt32(d);
+                if(dd>31)
+                {
+                    args.IsValid = false;
+                }
+                else
+                {
+                    string m=val.Substring(3,2);
+                    int mm=Convert.ToInt32(m);
+                    if(mm>12)
+                    {
+                        args.IsValid=false;
+                    }
+                    else
+                    {
+                        string y=val.Substring(6,4);
+                        //int yy=Convert.ToInt32(y);
+                        int yl = y.Length;
+                        if (yl == 4)
+                        {
+                            args.IsValid = true;
+                        }
+                        else
+                        {
+                            args.IsValid = false;
+                        }
+
+                    }
+                } 
+            }
+            else
+            {
+                args.IsValid=false ;
+            }
+           
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            Session["username"] = TextBox1.Text;
-            Session["email"] = TextBox2.Text;
-            Label1.Text = "registered successfully";
-
+           if(Page.IsValid)
+           {
+                Label1.Text = "DOB is" + TextBox1.Text;
+           }
+           else
+           {
+                Label1.Text="Page is not valid";
+;           }
         }
     }
 }
