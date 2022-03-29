32ef2d163f7047d62e1677d011398c2f900f7f23
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-29 22:06:41 +0530

    Add files via upload

---
+++ ItemController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication23.Models;
+
+namespace WebApplication23.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.ItemList = "Computer Shop Item List Page";
+
+            List<ItemList> ilist=new List<ItemList>();
+
+            {
+                new ItemList { Id = 1, Name = "iphone", Category = "phone", Price = 89000 };
+                new ItemList { Id = 2, Name = "Harddisk", Category = "computer", Price = 2999 };
+                new ItemList { Id = 3, Name = "Mouse", Category = "Computer", Price = 120 };
+                new ItemList { Id = 4, Name = "Samsung Note3", Category = "Mobile", Price = 9348 };
+            };
+
+
+
+            return View(ilist);
+        }
+    }
+}
