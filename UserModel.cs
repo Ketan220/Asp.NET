de545074e1465671344700c41a01228724e71c9f
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-01 21:45:56 +0530

    Add files via upload

---
+++ UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace WebApplication26.Models
+{
+    public class UserModel
+    {
+       
+        public string Name { get; set; }
+    }
+
+    
+}
