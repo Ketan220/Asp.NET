b4efbd8c2b62e40ebc0d6e2ae7db3a67c4250888
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-01 21:47:17 +0530

    Add files via upload

---
+++ Signup2Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication26.Models;
+
+namespace WebApplication26.Controllers
+{
+    public class Signup2Controller : Controller
+    {
+        // GET: Signup2
+        public ActionResult Index()
+        {
+            return View();
+        }
+         
+        [HttpPost]
+        public ActionResult Index(SignupModel2 u)
+        {
+            ViewBag.username=u.username;
+            ViewBag.password = u.password;
+            return View();
+        }
+    }
+}
