2c67a356aa33490c7c876f81cd224479f8675b11
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-01 21:46:49 +0530

    Add files via upload

---
+++ CalculateModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication26.Models
+{
+    public class CalculateModel
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+
+        public int result { get; set; } 
+    }
+}
