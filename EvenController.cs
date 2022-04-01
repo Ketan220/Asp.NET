650ca66fc73d07faacd5d76812bd5a91dc8eee60
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-01 21:47:55 +0530

    Add files via upload

---
+++ EvenController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication26.Models;
+
+namespace WebApplication26.Controllers
+{
+    public class EvenController : Controller
+    {
+        // GET: Even
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(EvenModel e)
+        {
+            int num1 = e.num;
+            string res;
+            if(num1%2==0)
+            {
+                 res="Number is Even";
+            }
+            else
+            {
+                res="Number is Odd";
+            }
+
+            ViewBag.result = res;
+            return View();
+
+        }
+    }
+}
