bf4676c8a088d20e0da3d57c71c647889065d852
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-01 21:47:30 +0530

    Add files via upload

---
+++ SampleController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication26.Models;
+
+namespace WebApplication26.Controllers
+{
+    public class SampleController : Controller
+    {
+        // GET: Sample
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserModel u)
+        {
+            ViewBag.Name = u.Name;
+            return View();
+        }
+
+    }
+}
