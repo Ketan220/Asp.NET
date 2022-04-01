9eec0ef9eccc07669143a65b13bceb9511a9c251
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-01 21:48:07 +0530

    Add files via upload

---
+++ CalculateController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication26.Models;
+
+namespace WebApplication26.Controllers
+{
+    public class CalculateController : Controller
+    {
+        // GET: Calculate
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Index(CalculateModel u)
+        {
+            int n1 = u.num1;
+            int n2 = u.num2;
+            int res=n1+n2;
+
+            ViewBag.result=res;
+            return View();
+        }
+    }
+}
