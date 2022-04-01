af43733b308840c8898643325cdeba96b79ddd04
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-01 21:46:38 +0530

    Add files via upload

---
+++ EvenModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication26.Models
+{
+    public class EvenModel
+    {
+        public int num { get; set; }             
+    }
+}
