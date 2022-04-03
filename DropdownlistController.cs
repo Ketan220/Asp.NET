56643b75a6924df2e3c4f4733aa6d6d442e08864
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:42:46 +0530

    Add files via upload

---
+++ DropdownlistController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication30.Models;
+
+namespace WebApplication30.Controllers
+{
+    public class DropdownlistController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(DropdownlistModel m)
+        {
+            var selectedValue = m.SelectTeaType;
+            ViewBag.TeaType = selectedValue.ToString();
+            return View();
+        }
+
+    }
+}
