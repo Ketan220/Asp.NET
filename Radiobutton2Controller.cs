bafb70f0d5bd9c4ecd994feac6c3da5547991889
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:38:49 +0530

    Add files via upload

---
+++ Radiobutton2Controller.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication29.Models;
+
+namespace WebApplication29.Controllers
+{
+    public class Radiobutton2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Radiobutton2Model r)
+        {
+            int bal = 1000;
+           
+             int balance = bal + r.amt;
+             ViewBag.Balance = balance;
+
+          
+              int withdraw = balance - r.amt;
+              ViewBag.withdraw = withdraw;
+
+
+           
+            
+                
+              
+
+            return View();
+        }
+    }
+}
