c2ffecbc289791540cde96ccfe55e5fa4c062907
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:53:21 +0530

    Add files via upload

---
+++ WeaklyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication32.Controllers
+{
+    public class WeaklyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult form1(int txtId, string txtName, string chkAddon)
+        {
+            ViewBag.Id = txtId;
+            ViewBag.Name = txtName;
+            if (chkAddon != null)
+                ViewBag.Addon = "Selected";
+            else
+                ViewBag.Addon = "Not Selected";
+
+            return View("Index");
+        }
+
+    }
+}
