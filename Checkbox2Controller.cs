ce021c28ac1a773bb34407c412a5998e467af240
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:38:25 +0530

    Add files via upload

---
+++ Checkbox2Controller.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication29.Models;
+
+namespace WebApplication29.Controllers
+{
+    public class Checkbox2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Checkbox2Model cb)
+        {
+            int n1 = cb.num1;
+            int n2 = cb.num2;
+            
+            if(cb.add==true)
+            {
+                ViewBag.add = n1 +n2;
+            }
+            if(cb.sub==true)
+            {
+                ViewBag.sub = n1-n2;    
+            }
+            if(cb.mul==true)
+            {
+                ViewBag.mul = n1*n2;    
+            }
+            return View();  
+        }
+    }
+}
