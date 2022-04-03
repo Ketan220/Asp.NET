0b3e1fdb68957efa3770be69e09a23bd1e5aff8a
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:39:00 +0530

    Add files via upload

---
+++ Radiobutton3Controller.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication29.Models;
+
+namespace WebApplication29.Controllers
+{
+    public class Radiobutton3Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult sayhello(Radiobutton3Model r)
+        {
+
+            ViewBag.city=r.city;
+
+            int count = 0;
+            int res = 0;
+            if(r.delhi==true)
+            {
+               res = count++;
+            }
+            else if(r.mumbai==true)
+            {
+                res=count++;    
+            }
+            else if(r.bhopal==true)
+            {
+                res = count++;
+            }
+            ViewBag.res=res;
+
+            return View();
+        }
+
+
+    }
+}
