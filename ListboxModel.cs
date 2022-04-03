d8a7969e0a13800796a26fac67fb883d4bd9f9ef
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:46:19 +0530

    Add files via upload

---
+++ ListboxModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+
+namespace WebApplication31.Models
+{
+    public class ListboxModel
+    {
+        public int[] SelectedTeaIds { get; set; }
+        public IEnumerable<SelectListItem> TeaList { get; set; }
+
+    }
+}
