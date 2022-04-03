36b5a50438dfd1bdf0a1931a563c988651898952
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:47:01 +0530

    Add files via upload

---
+++ EditorforController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication31.Models;
+
+namespace WebApplication31.Controllers
+{
+    public class EditorforController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(EditorforModel u)
+        {
+            ViewBag.Username = u.UserName;
+            ViewBag.Age=u.Age;
+            ViewBag.City=u.City;
+           
+            if(u.isHandicapped==true)
+            {
+                ViewBag.hd = "I'm Handicapped";
+            }
+            else
+            {
+                ViewBag.hd = "I'm not handicapped";
+            }
+            return View(); 
+
+        }
+    }
+}
