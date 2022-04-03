0c70c4bd8f76f8f189c88426e38bc00fb01a3df5
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:54:56 +0530

    Add files via upload

---
+++ ClientsideModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+
+namespace WebApplication33.Models
+{
+    public class ClientsideModel
+    {
+        [Required(ErrorMessage ="Name is Required")]
+        [StringLength(10, MinimumLength = 3)]
+        public string Name { get; set; }
+     
+    }
+}
