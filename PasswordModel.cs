ef7b55c5c506d3ed946583a7be31d7fda51a3497
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:41:51 +0530

    Add files via upload

---
+++ PasswordModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebApplication30.Models
+{
+    public class PasswordModel
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is Required.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirmation Password is Required")]
+        [Compare("Password", ErrorMessage = "Password Must Match")]
+        public string ConfirmPassword { get; set; }
+
+
+    }
+
+
+}
