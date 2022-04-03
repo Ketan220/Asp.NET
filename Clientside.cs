3d60da608947ff4a3adb699ed95e2026c5f7cb87
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:56:48 +0530

    Add files via upload

---
+++ Clientside.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace WebApplication34.Models
+{
+    [Bind(Exclude = "Id")]
+    public class Clientside
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is Required")]
+        [StringLength(10, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email ID is Required")]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(50)]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Incorrect Email Format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Confirm Email is Required")]
+        [DataType(DataType.EmailAddress)]
+        [System.ComponentModel.DataAnnotations.Compare("Email", ErrorMessage = "Email Not Matched")]
+        public string ConfirmEmail { get; set; }
+
+        [Required(ErrorMessage = "Age is Required")]
+        [Range(1, 120, ErrorMessage = "Age must be between 1-120 in years.")]
+        public int Age { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm:ss tt}")]
+        public System.DateTime? HireDate { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{2})[-. ]?([0-9]{4})[-. ]?([0-9]{3})[-. ]?([0-9]{3})$", ErrorMessage = "Not a valid Phone number")]
+        public string Pno { get; set; }
+
+
+
+    }
+
+
+}
