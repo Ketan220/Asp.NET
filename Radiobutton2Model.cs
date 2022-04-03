c74927e0da81bfe08d6279dc37a7d5f6f82787a3
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:37:31 +0530

    Add files via upload

---
+++ Radiobutton2Model.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication29.Models
+{
+    public class Radiobutton2Model
+    {
+        public int actno { get; set; }
+
+        public int amt { get; set; }
+        public bool balance { get; set; }
+        public bool withdraw { get; set; }
+
+    }
+}
