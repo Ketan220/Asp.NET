ba6695f789825ad711606a21478cbaedc43f91d8
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:58:08 +0530

    Add files via upload

---
+++ CalModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication35.Models
+{
+    public class CalModel
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+
+        public int result { get; set; }
+    }
+}
