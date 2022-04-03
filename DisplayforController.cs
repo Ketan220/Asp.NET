f1ea74e39c6292880b36927c1610964701207bde
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:42:35 +0530

    Add files via upload

---
+++ DisplayforController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication30.Models;
+
+namespace WebApplication30.Controllers
+{
+    public class DisplayforController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection fc, DisplayforModel um)
+        {
+            if (ModelState.IsValid)
+            {
+                um.UserName = fc["UserName"];
+                return View(um);
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+    }
+}
