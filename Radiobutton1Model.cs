d3ee7ddef44bbe977d0617eae32b44b885c2914c
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:37:18 +0530

    Add files via upload

---
+++ Radiobutton1Model.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication29.Models
+{
+    public class Radiobutton1Model
+    {
+        public string TeaType { get; set; }
+        public string HotelType { get; set; }
+
+    }
+}
