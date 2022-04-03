e567c49f8c96b5087da7781088339fb40275e8a9
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:51:45 +0530

    Add files via upload

---
+++ StronglyModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication32.Models
+{
+    public class StronglyModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Addon { get; set; }
+    }
+}
