a2a69e7559482db31e78d99af9236bf06db85bf2
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:52:57 +0530

    Add files via upload

---
+++ StronglyController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication32.Controllers
+{
+    public class StronglyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Form2(Models.StronglyModel sm)
+        {
+            ViewBag.Id = sm.Id;
+            ViewBag.Name = sm.Name;
+            if (sm.Addon == true)
+                ViewBag.Addon = "Selected";
+            else
+                ViewBag.Addon = "Not Selected";
+
+            return View("Index");
+        }
+
+    }
+}
