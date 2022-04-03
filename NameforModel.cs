9e65fcdbe4f1f17c78ecea365aa34225dfb06b3d
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:46:35 +0530

    Add files via upload

---
+++ NameforModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication31.Models
+{
+    public class NameforModel
+    {
+        public string UserName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+
+    }
+}
