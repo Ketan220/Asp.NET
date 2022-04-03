c8119c40110cb166ec4e9e058b47e245fd957d39
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:43:22 +0530

    Add files via upload

---
+++ TextareaforController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication30.Models;
+
+namespace WebApplication30.Controllers
+{
+    public class TextareaforController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(TextareaforModel u)
+        {
+            ViewBag.name = u.Name;
+            return View();
+        }
+        
+    }
+}
