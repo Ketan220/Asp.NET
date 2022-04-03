cef1da004afcab60f5e1944b028bcd5b39bcf4de
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:43:11 +0530

    Add files via upload

---
+++ PasswordController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication30.Models;
+
+namespace WebApplication30.Controllers
+{
+    public class PasswordController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection fc, PasswordModel um)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.BasicPassword = fc["BasicPassword"];
+
+                ViewBag.StrongPassword = um.ConfirmPassword;
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
