26308fe67590d8f3487407aef80802d7c785e3c0
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:37:43 +0530

    Add files via upload

---
+++ Radiobutton3Model.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication29.Models
+{
+    public class Radiobutton3Model
+    {
+        public string city { get; set; }
+
+        public string city1 { get; set; }
+        public string city2 { get; set; }
+
+        public bool delhi { get; set; } 
+        public bool mumbai { get; set; }    
+        public bool bhopal { get; set; }    
+
+    }
+}
