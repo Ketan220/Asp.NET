e500866a2226d87b793da06369a25ee3b49a6b7f
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:34:18 +0530

    Add files via upload

---
+++ MarksModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication27.Models
+{
+    public class MarksModel
+    {
+        public int marks1 { get; set; }
+        public int marks2 { get; set; }
+
+        public int marks3 { get; set; }
+
+        public int result { get; set; }
+
+        
+    }
+}
