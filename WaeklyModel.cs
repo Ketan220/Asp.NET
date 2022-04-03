137d260c78536fcfb1973891b616322a77078a81
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:52:39 +0530

    Add files via upload

---
+++ WaeklyModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication32.Models
+{
+    public class WaeklyModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Adon { get; set; }  
+    }
+}
