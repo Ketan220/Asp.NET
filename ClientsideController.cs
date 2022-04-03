7ae90a6292caa38a47414fd685b4ca2f47ab264d
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:57:09 +0530

    Add files via upload

--- ClientsideController.cs
+++ ClientsideController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication34.Models;
 
-namespace WebApplication33.Controllers
+namespace WebApplication34.Controllers
 {
     public class ClientsideController : Controller
     {
-        public IActionResult Index()
+        // GET: Clientside
+        public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult abc(Clientside cm)
+        {
+            if(ModelState.IsValid)
+            {
+                ViewBag.name = cm.Name;
+                ViewBag.email = cm.Email;
+                ViewBag.age = cm.Age;
+                ViewBag.hiredate = cm.HireDate;
+                ViewBag.pno = cm.Pno;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.name = "No Data";
+                ViewBag.email = "No Data";
+                ViewBag.age = "No Data";
+                ViewBag.pno = "No Data";
+                return View("Index");
+            }
         }
     }
 }
