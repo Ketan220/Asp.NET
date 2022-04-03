8ed0e5023c3365c800936be5cefd8a754abb3097
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:51:55 +0530

    Add files via upload

---
+++ StronglytypedModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication32.Models
+{
+    public class StronglytypedModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Addon { get; set; }
+    }
+}
