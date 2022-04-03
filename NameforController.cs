3ce244daf2ad701be6c00d99432ded20b7596aec
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:47:52 +0530

    Add files via upload

---
+++ NameforController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication31.Controllers
+{
+    public class NameforController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
