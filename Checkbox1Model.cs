f824b914a6bb47886861642ee520597091917892
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:36:40 +0530

    Add files via upload

---
+++ Checkbox1Model.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication29.Models
+{
+    public class Checkbox1Model
+    {
+        public bool Tea { get; set; }  
+        public bool Loundry { get; set; }  
+        public bool Breakfast { get; set; }  
+
+    }
+}
