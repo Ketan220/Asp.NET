7bd9c331e0c4e0ecf0fe99c446222fa5b31f80bf
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:45:27 +0530

    Add files via upload

---
+++ EditorforModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication31.Models
+{
+    public class EditorforModel
+    {
+        public string UserName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public bool isHandicapped { get; set; }
+
+    }
+}
