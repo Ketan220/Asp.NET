9d559e9ef7981fd7e90a939991cd900880a16e19
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:37:05 +0530

    Add files via upload

---
+++ Checkbox2Model.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication29.Models
+{
+    public class Checkbox2Model
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public bool add { get; set; }
+        public bool sub { get; set; }
+        public bool mul { get; set; }
+    }
+}
