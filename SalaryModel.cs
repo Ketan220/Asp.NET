47c504350ba67204ea1c215cbb93907289c25f6a
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:58:20 +0530

    Add files via upload

---
+++ SalaryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication35.Models
+{
+    public class SalaryModel
+    {
+        public int empno { get; set; }
+        public string empname { get; set; }
+        public float bs { get; set; }   
+    }
+}
