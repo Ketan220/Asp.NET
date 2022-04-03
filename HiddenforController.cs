1a644ed91159baa1302a1ecb8ab3683c32f06b6f
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:47:12 +0530

    Add files via upload

---
+++ HiddenforController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication31.Models;
+
+namespace WebApplication31.Controllers
+{
+    public class HiddenforController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection fc, HiddenforModel um)
+        {
+            ViewBag.basicname = fc["BasicHiddenName"];
+
+            ViewBag.id = um.UserId;
+            ViewBag.name = um.UserName;
+            return View(um);
+        }
+
+    }
+}
