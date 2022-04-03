d443df7fbaccaa38fbca32f2ae3ff4bb7b629775
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:38:37 +0530

    Add files via upload

---
+++ Radiobutton1Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication29.Models;
+
+namespace WebApplication29.Controllers
+{
+    public class Radiobutton1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Radiobutton1Model u)
+        {
+            ViewBag.TeaType=u.TeaType;
+            ViewBag.HotelType=u.HotelType;
+            return View();
+        }
+    }
+}
