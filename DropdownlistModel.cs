38a6bda2c9715379602f631292305fca03cde624
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-03 19:41:36 +0530

    Add files via upload

---
+++ DropdownlistModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication30.Models
+{
+    public enum TeaType
+    {
+        Tea, Coffee, GreenTea, BlackTea
+    }
+
+    public class DropdownlistModel
+    {
+        public TeaType SelectTeaType { get; set; }
+    }
+
+}
