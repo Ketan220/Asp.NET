37ab974317b044e3fef1dc0df0ae53ac0bf7de2e
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-05 20:59:02 +0530

    Add files via upload

--- SignupModel.cs
+++ SignupModel.cs
@@ -1,6 +1,16 @@
-﻿namespace WebApplication30.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication37.Models
 {
     public class SignupModel
     {
+        public int userid { get; set; }
+        public String uname { get; set; }
+        public String upass { set; get; }
+        public String course { set; get; }
+        public String city { set; get; }
     }
 }
