bf99ca78eaf4543251c98b1cd21e0d850f6b4dfd
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-06 21:36:28 +0530

    Add files via upload

---
+++ StudentContext.cs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApplication39.Models
+{
+    public class StudentContext:DbContext
+    {
+        public  StudentContext():base("studsDatabse")
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }    
+            
+    }
+}
