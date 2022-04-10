0c8c815c7b73521b12a3184a4cb8009dcaab1c10
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-10 20:28:12 +0530

    Add files via upload

---
+++ Class6.cs
@@ -0,0 +1,24 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Class6
+    {
+        delegate void demo();
+
+        public static void Main()
+        {
+            demo d = () =>
+            {
+                Console.WriteLine("Welcome to mkpits");
+                Console.WriteLine("again welcome to mkplacements");
+            };
+            d();
+            Console.ReadKey();  
+        }
+    }
+}*/
