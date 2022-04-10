82033b186be25e67ea71680fb470d79dbf562342
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-10 20:27:33 +0530

    Add files via upload

---
+++ Class3.cs
@@ -0,0 +1,34 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class employee
+    {
+        public int empid { get; set; }
+        public string empname { get; set; }
+        public double salary { get; set; }
+
+    }
+
+    internal class Class3
+    {
+        public delegate double empdelegate(employee emp);
+        static void Main(string[] args)
+        {
+            //creating an object of employee class
+            employee e = new employee() { salary = 25000 };
+            //creating a lamda expression
+            empdelegate ed = s => s.salary;
+            //calling 
+            double res = ed(e);
+            Console.WriteLine("emp salary is " + res);
+            Console.ReadKey();
+
+        }
+
+    }
+}*/
