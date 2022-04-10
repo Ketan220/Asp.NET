dcbfeb35f20400d1db12fc924ae5c140fb251029
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-10 20:27:44 +0530

    Add files via upload

---
+++ Class4.cs
@@ -0,0 +1,29 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Student
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    internal class Class4
+    {
+        delegate bool IsYoungerThan(Student stud, int youngerage);
+
+        public static void Main()
+        {
+            IsYoungerThan isyoungethan = (s, youngerage) => s.Age < youngerage;
+            Student stud = new Student() { Age = 21 };
+            Console.WriteLine(isyoungethan(stud, 26));
+
+        }
+    }
+}*/
