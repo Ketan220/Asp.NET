6ce37c9835ebd2c89082da16d25a043328211344
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-10 20:27:07 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Student
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    internal class Class1
+    {
+        delegate bool ISTeenAger(Student stud);
+
+        public static void Main()
+        {
+            ISTeenAger isteenager = delegate (Student s)
+            {
+                 Console.WriteLine("id = " + s.Id);
+                 Console.WriteLine("Name  = " + s.Name);
+                 Console.WriteLine("age = " + s.Age);
+                 return s.Age > 12 && s.Age < 20;
+
+            };
+            //creating an object of student class
+            Student stud = new Student() { Id = 123, Name = "ravi", Age = 18 };
+
+            //calling the anonymouse function or method
+            bool res = isteenager(stud);
+            Console.WriteLine(" is teenager : " + res);
+            Console.ReadKey();
+
+        }
+
+    }
+}*/
