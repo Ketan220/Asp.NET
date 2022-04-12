10a2f9e57cd6f8f7cc2172185c2b55932d0c5a77
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-12 19:49:27 +0530

    Add files via upload

--- WebForm3.aspx.cs
+++ WebForm3.aspx.cs
@@ -4,14 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
 
-namespace WebApplication16
+namespace WebApplication43
 {
     public partial class WebForm3 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc=new XmlDocument();
+            doc.Load(Server.MapPath("~/Tenders.xml"));
+            XmlNodeList ilist = doc.GetElementsByTagName("Activity");
+
+            for(int i = 0;i<ilist.Count;i++)
+            {
+                ListBox1.Items.Add(ilist[i].InnerXml);
+            }
         }
     }
 }
