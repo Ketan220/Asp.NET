e8f9e3f4f7d3fe0c91eefdbc716aae61b5dcc9ff
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-12 19:48:52 +0530

    Add files via upload

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -4,14 +4,51 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Xml;
 
-namespace WebApplication16
+namespace WebApplication43
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            if (!IsPostBack)
+            {
+                BindDataToGridview();
+            }
+        }
+        // This method is used to bind data to dropdownlist and gridview  
+        protected void BindDataToGridview()
+        {
+            //open the tender xml file  
+            XmlTextReader xmlreader = new XmlTextReader(Server.MapPath("~/Tenders.xml"));
+            //reading the xml data  
+            DataSet ds = new DataSet();
+            ds.ReadXml(xmlreader);
+            xmlreader.Close();
+            //if ds is not empty  
+            if (ds.Tables.Count != 0)
+            {
+                //Bind Data to gridview  
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+                // Bind Data to dropdownlist  
+
+                DropDownList1.DataSource = ds;
+                DropDownList1.DataTextField = "BillNo";
+                DropDownList1.DataValueField = "BillNo";
+                DropDownList1.DataBind();
+                DropDownList2.DataSource = ds;
+                DropDownList2.DataTextField = "PageNo";
+                DropDownList2.DataValueField = "PageNo";
+                DropDownList2.DataBind();
+                DropDownList3.DataSource = ds;
+                DropDownList3.DataTextField = "Activity";
+                DropDownList3.DataValueField = "Activity";
+                DropDownList3.DataBind();
+            }
         }
     }
 }
