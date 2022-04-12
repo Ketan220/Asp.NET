1ef313c5afcea0091b6fce8b75978f82e31d465f
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-04-12 19:49:12 +0530

    Add files via upload

--- WebForm2.aspx.cs
+++ WebForm2.aspx.cs
@@ -4,13 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
 
-namespace WebApplication16
+namespace WebApplication43
 {
     public partial class WebForm2 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            XmlReader xReader=XmlReader.Create(Server.MapPath("~/Tenders.xml"));
+            while(xReader.Read())
+            {
+                switch(xReader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        ListBox1.Items.Add("<"+xReader.Name+">");
+                        break;
+                    case XmlNodeType.Text:
+                        ListBox1.Items.Add(xReader.Value);
+                        break;
+                    case XmlNodeType.EndElement:
+                        ListBox1.Items.Add("<" + xReader.Name + ">");
+                        break ;
+                }
+            }
 
         }
     }
